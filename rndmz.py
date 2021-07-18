81e83aaefcfda7ff1f6a32f6cbf0126a9f0677c2
XENOGENESIS079 <66459893+XENOGENESIS079@users.noreply.github.com>
2021-07-18 22:17:03 +0300

    Add files via upload

---
+++ rndmz.py
@@ -0,0 +1,18 @@
+from pynput import mouse
+import keyboard
+import random
+
+#ЯЧЕЙКИ ИНВЕНТАРЯ, БЛОКИ КОТОРЫХ НАДО РАНДОМИТЬ
+cells = ['1', '2', '3', '4']
+#КОЭФФИЦИЕНТЫ СООТНОШЕНИЯ БЛОКОВ
+factor = [1.0, 1.0, 1.0, 1.0]
+const = 10
+fac_cells = list(map(int, "".join([str(x)*int(factor[cells.index(x)]*const) for x in cells])))
+
+def mouse_click(left, right, button, pressed):
+    if pressed and button == mouse.Button.right:
+        keyboard.press_and_release(str(fac_cells[random.randint(0, len(fac_cells)-1)]))
+       
+with mouse.Listener(on_click=mouse_click) as listener:
+    listener.join()
+
